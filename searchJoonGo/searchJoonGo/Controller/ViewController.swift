9e8198e25c68c95bafd33fdc6c02322eae567671
강병우 <kbw2204@icloud.com>
2019-10-30 13:33:24 +0900

    Initial commit

---
+++ searchJoonGo/searchJoonGo/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  searchJoonGo
+//
+//  Created by 강병우 on 30/10/2019.
+//  Copyright © 2019 강병우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
